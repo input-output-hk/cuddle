ce835eb52a0039443faf0e7d36cd4dda3da800d9
Nicholas Clarke <nick@topos.org.uk>
2023-11-13 13:45:36 +0100

    Add some additional tests.
    Also refactor the testing.

--- test/Main.hs
+++ test/Main.hs
@@ -12,26 +12,33 @@ import Text.Megaparsec
 main :: IO ()
 main = hspec $
   describe "cddlParser" $ do
-    describe "pValue" $ do
-      it "Parses integer" $
-        parse pValue "" "123" `shouldParse` VNum 123
-      it "Parses text" $
-        parse pValue "" "\"Hello World\"" `shouldParse` VText "Hello World"
-    describe "pOccur" $ do
-      it "Parses OneOrMore" $
-        parse pOccur "" "+" `shouldParse` OIOneOrMore
-      it "Parses ZeroOrMore" $
-        parse pOccur "" "*" `shouldParse` OIZeroOrMore
-      it "Parses Optional" $
-        parse pOccur "" "?" `shouldParse` OIOptional
-      it "Parses Lower Bounded" $
-        parse pOccur "" "3*" `shouldParse` OIBounded (Just 3) Nothing
-      it "Parses Upper Bounded" $
-        parse pOccur "" "*9" `shouldParse` OIBounded Nothing (Just 9)
-      it "Parses bounded on both sides" $
-        parse pOccur "" "3*9" `shouldParse` OIBounded (Just 3) (Just 9)
+    valueSpec
+    occurSpec
     nameSpec
     type2Spec
+    grpEntrySpec
+
+valueSpec :: Spec
+valueSpec = describe "pValue" $ do
+  it "Parses integer" $
+    parse pValue "" "123" `shouldParse` VNum 123
+  it "Parses text" $
+    parse pValue "" "\"Hello World\"" `shouldParse` VText "Hello World"
+
+occurSpec :: Spec
+occurSpec = describe "pOccur" $ do
+  it "Parses OneOrMore" $
+    parse pOccur "" "+" `shouldParse` OIOneOrMore
+  it "Parses ZeroOrMore" $
+    parse pOccur "" "*" `shouldParse` OIZeroOrMore
+  it "Parses Optional" $
+    parse pOccur "" "?" `shouldParse` OIOptional
+  it "Parses Lower Bounded" $
+    parse pOccur "" "3*" `shouldParse` OIBounded (Just 3) Nothing
+  it "Parses Upper Bounded" $
+    parse pOccur "" "*9" `shouldParse` OIBounded Nothing (Just 9)
+  it "Parses bounded on both sides" $
+    parse pOccur "" "3*9" `shouldParse` OIBounded (Just 3) (Just 9)
 
 -- it "result of parsing satisfies what it should" $
 --   parse myParser "" "aaaa" `parseSatisfies` ((== 4) . length)
@@ -77,3 +84,66 @@ type2Spec = describe "type2" $ do
                   ]
               )
           )
+  describe "Array" $ do
+    it "Parses an array with an alternative" $
+      parse pType2 "" "[int // string]"
+        `shouldParse` T2Array
+          ( Group
+              ( [ GEType
+                    Nothing
+                    Nothing
+                    ( Type0
+                        ( Type1
+                            (T2Name (Name "int") Nothing)
+                            Nothing
+                            NE.:| []
+                        )
+                    )
+                ]
+                  NE.:| [ [ GEType
+                              Nothing
+                              Nothing
+                              ( Type0
+                                  ( Type1
+                                      (T2Name (Name "string") Nothing)
+                                      Nothing
+                                      NE.:| []
+                                  )
+                              )
+                          ]
+                        ]
+              )
+          )
+
+    it "Parses an array with a value alternative" $
+      parse pType2 "" "[0 // 1]"
+        `shouldParse` T2Array
+          ( Group
+              ( [ GEType
+                    Nothing
+                    Nothing
+                    (Type0 (NE.singleton (Type1 (T2Value (VNum 0)) Nothing)))
+                ]
+                  NE.:| [ [ GEType
+                              Nothing
+                              Nothing
+                              (Type0 (NE.singleton (Type1 (T2Value (VNum 0)) Nothing)))
+                          ]
+                        ]
+              )
+          )
+
+grpEntrySpec :: SpecWith ()
+grpEntrySpec = describe "GroupEntry" $ do
+  it "Prefers GEType over GERef for names" $
+    parse pGrpEntry "" "int"
+      `shouldParse` GEType
+        Nothing
+        Nothing
+        ( Type0
+            ( Type1
+                (T2Name (Name "int") Nothing)
+                Nothing
+                NE.:| []
+            )
+        )
