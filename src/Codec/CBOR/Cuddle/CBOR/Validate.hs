1a6a5517daa4a905893e9099c1844615e978b80e
Javier Sagredo <jasataco@gmail.com>
2025-04-15 13:18:26 +0200

    Most of the other operators

--- src/Codec/CBOR/Cuddle/CBOR/Validate.hs
+++ src/Codec/CBOR/Cuddle/CBOR/Validate.hs
@@ -1,21 +1,26 @@
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-matches #-}
 
 module Codec.CBOR.Cuddle.CBOR.Validate where
 
+import Codec.CBOR.Cuddle.CBOR.Validate.Combinators
+import Codec.CBOR.Cuddle.CBOR.Validate.Ops
+import Codec.CBOR.Cuddle.CBOR.Validate.Types
 import Codec.CBOR.Cuddle.CDDL hiding (Group (..))
 import Codec.CBOR.Cuddle.CDDL.CTree
 import Codec.CBOR.Cuddle.CDDL.CtlOp
 import Codec.CBOR.Cuddle.CDDL.Postlude
 import Codec.CBOR.Cuddle.CDDL.Resolve
+import Codec.CBOR.Magic
 import Codec.CBOR.Read
 import Codec.CBOR.Term
 import Control.Lens ((#))
 import Control.Monad (unless, when)
 import Control.Monad.Except
+import Data.Bits (Bits, bit, complement, zeroBits, (.&.))
 import Data.ByteString qualified as BS
 import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Lazy qualified as BSL
@@ -31,7 +36,6 @@ import Data.Text.Lazy qualified as TL
 import Data.Validation
 import Data.Word
 import Debug.Trace
-import Data.ByteString.Base16 qualified as Base16
 
 -- We make the conscious choice of parsing the provided term instead
 -- of navigating the rule, as rules might be circular but the term
@@ -39,81 +43,8 @@ import Data.ByteString.Base16 qualified as Base16
 
 -- TODOs
 -- - cuts in maps
--- - .bits
--- - .regexp
 -- - .default
--- - Enum
 -- - Unwrap
--- - maps and lists .eq and .ne
-
-type CDDL' = CTreeRoot' Identity MonoRef
-type Rule' = Node MonoRef
-
-failUnless :: Bool -> e -> Validation e ()
-failUnless cond = validateWithUnless cond (_Success # ())
-
-validateWithUnless :: Bool -> Validation e () -> e -> Validation e ()
-validateWithUnless cond val msg
-  | cond = val
-  | otherwise = _Failure # msg
-
-resolveIfRef :: CTreeRoot' Identity MonoRef -> Node MonoRef -> CTree MonoRef
-resolveIfRef _ (MIt aa) = aa
-resolveIfRef ct@(CTreeRoot cddl) (MRuleRef n) = do
-  case Map.lookup n cddl of
-    Nothing -> error $ "Unbound reference: " <> show n
-    Just val -> resolveIfRef ct $ runIdentity val
-
-{-------------------------------------------------------------------------------
-  Operators
--------------------------------------------------------------------------------}
-
-runNumericOp ::
-  (Ord a, Show a) =>
-  String -> CtlOp -> a -> a -> Validation [Reason] ()
-runNumericOp t op i i' = case op of
-  Lt -> failUnless (i < i') [Unexpected ("a " <> t <> " < " <> show i') (show i)]
-  Le -> failUnless (i <= i') [Unexpected ("a " <> t <> " <= " <> show i') (show i)]
-  Gt -> failUnless (i > i') [Unexpected ("a " <> t <> " > " <> show i') (show i)]
-  Ge -> failUnless (i >= i') [Unexpected ("a " <> t <> " >= " <> show i') (show i)]
-  Eq -> failUnless (i == i') [Unexpected ("a " <> t <> " == " <> show i') (show i)]
-  Ne -> failUnless (i /= i') [Unexpected ("a " <> t <> " /= " <> show i') (show i)]
-  _ -> _Failure # [DidNotValidate $ "Op " <> show op <> " is not applicable to type " <> t]
-
-runStringOp ::
-  (Ord a, Show a) =>
-  String -> CtlOp -> a -> (a -> Int) -> Either Int a -> Validation [Reason] ()
-runStringOp t op i getSize i' = case (op, i') of
-  (Size, Left sz') ->
-    let sz = getSize i
-     in failUnless
-          (sz == sz')
-          [Unexpected ("a " <> t <> " of length " <> show sz') ("a " <> t <> " of length " <> show sz)]
-  (_, Right i'') -> runNumericOp t op i i''
-  _ -> _Failure # [DidNotValidate $ "Op " <> show op <> " is not applicable to type " <> t]
-
-runBytesOp ::
-  CDDL' -> CtlOp -> BS.ByteString -> Rule' -> Validation [Reason] ()
-runBytesOp cddl op bs rule = case op of
-  Cbor ->
-    case runExcept $
-          liftEither $ deserialiseFromBytes decodeTerm (BSL.fromStrict bs) of
-      Left _err -> _Failure # [InvalidInnerCBOR]
-      Right (rest, term) ->
-       failUnless (BSL.null rest) [NotASingleInnerTerm] *>
-       doValidate cddl term rule
-  Cborseq ->
-    case runExcept $
-          liftEither $ deserialiseFromBytes decodeTerm (traceShowWith (Base16.encode . BSL.toStrict) $ BSL.snoc (BSL.cons 0x9f $ BSL.fromStrict bs) 0xff) of
-      Right (rest, TListI xs) ->
-       failUnless (BSL.null rest) [NotASingleInnerTerm] *>
-       L.foldl1' (<!>) [ doValidate cddl t rule | t <- xs ]
-      _ -> _Failure # [InvalidInnerCBOR]
-  Bits -> undefined
-  _ -> maybe
-        (_Failure # [DidNotValidate $ "Control rhs is not an int or bytestring: " <> show rule])
-        (runStringOp "bytestring" op bs BS.length)
-        (getTheIntOrByteString cddl rule)
 
 {-------------------------------------------------------------------------------
   Choices
@@ -129,20 +60,9 @@ validateChoices cddl t (choice : choices) =
   Int and integers
 -------------------------------------------------------------------------------}
 
-getTheInt :: CDDL' -> Rule' -> Maybe Int
-getTheInt cddl ref = case resolveIfRef cddl ref of
-  Literal (VUInt i) -> Just (fromIntegral i)
-  Literal (VNInt i) -> Just (-fromIntegral @Word64 @Int i)
-  _ -> Nothing
-
-getTheInteger :: CDDL' -> Rule' -> Maybe Integer
-getTheInteger cddl ref = case resolveIfRef cddl ref of
-  Literal (VBignum i) -> Just i
-  _ -> Nothing
-
 validateIntegral ::
-  (Integral a, Show a) => CDDL' -> a -> (CDDL' -> Rule' -> Maybe a) -> Rule' -> Validation [Reason] ()
-validateIntegral cddl i getInner theRule = case traceShowId (resolveIfRef cddl theRule, getInner cddl theRule) of
+  (Integral a, Show a, Ops a) => CDDL' -> a -> Rule' -> Validation [Reason] ()
+validateIntegral cddl i theRule = case traceShowId (resolveIfRef cddl theRule, get cddl theRule) of
   (_, Just i') ->
     failUnless (i == i') [Unexpected ("the fixed int " <> show i') (show i)]
   (Postlude PTInt, Nothing) -> _Success # ()
@@ -150,7 +70,7 @@ validateIntegral cddl i getInner theRule = case traceShowId (resolveIfRef cddl t
     failUnless (i >= 0) [Unexpected "a positive int" (show i)]
   (Postlude PTNInt, Nothing) ->
     failUnless (i < 0) [Unexpected "a negative int" (show i)]
-  (Range from to incl, Nothing) -> case (getInner cddl from, getInner cddl to) of
+  (Range from to incl, Nothing) -> case (get cddl from, get cddl to) of
     (Just low, Just high) ->
       failUnless
         ( low <= i && case incl of
@@ -166,12 +86,8 @@ validateIntegral cddl i getInner theRule = case traceShowId (resolveIfRef cddl t
     Postlude PTBytes -> _Success # ()
     _ -> _Failure # [DidNotValidate $ "Malformed rule! " <> show (Tag 3 theRule')]
   (Control op n ctrller, Nothing) ->
-    let intControl =
-          maybe
-            (_Failure # [DidNotValidate $ "Control rhs is not a single int: " <> show ctrller])
-            (runNumericOp "int" op i)
-            $ getInner cddl ctrller
-     in case (resolveIfRef cddl n, getInner cddl n) of
+    let intControl = dispatchOp op doValidate cddl ctrller i
+     in case (resolveIfRef cddl n, get cddl n) of
           (_, Just i') ->
             validateWithUnless
               (i == i')
@@ -180,7 +96,7 @@ validateIntegral cddl i getInner theRule = case traceShowId (resolveIfRef cddl t
           (Postlude PTInt, Nothing) -> intControl
           (Postlude PTUInt, Nothing) ->
             validateWithUnless (i >= 0) intControl [DidNotValidate "Expecting a positive int"]
-          (Range from to incl, Nothing) -> case (getInner cddl from, getInner cddl to) of
+          (Range from to incl, Nothing) -> case (get cddl from, get cddl to) of
             (Just low, Just high) ->
               validateWithUnless
                 ( low <= i && case incl of
@@ -199,19 +115,13 @@ validateIntegral cddl i getInner theRule = case traceShowId (resolveIfRef cddl t
   ByteStrings
 -------------------------------------------------------------------------------}
 
-getTheIntOrByteString :: CDDL' -> Rule' -> Maybe (Either Int BS.ByteString)
-getTheIntOrByteString cddl rule = case resolveIfRef cddl rule of
-  Literal (VUInt i) -> Just (Left $ fromIntegral i)
-  Literal (VBytes b) -> Just (Right b)
-  _ -> Nothing
-
 validateBytes :: CDDL' -> BS.ByteString -> Rule' -> Validation [Reason] ()
 validateBytes cddl b theRule = case resolveIfRef cddl theRule of
   Literal (VBytes (Base16.decode -> Right b')) ->
     failUnless (b == b') [Unexpected ("the exact bytestring " <> show b') (show b)]
   Postlude PTBytes -> _Success # ()
   Control op n ctrller ->
-    let bytesControl = runBytesOp cddl op b ctrller
+    let bytesControl = dispatchOp op doValidate cddl ctrller b
      in case resolveIfRef cddl n of
           Postlude PTBytes -> bytesControl
           lhs -> _Failure # [DidNotValidate $ "Control lhs is not valid for bytestrings: " <> show lhs]
@@ -221,28 +131,13 @@ validateBytes cddl b theRule = case resolveIfRef cddl theRule of
   Text
 -------------------------------------------------------------------------------}
 
-getTheIntOrText :: CDDL' -> Rule' -> Maybe (Either Int T.Text)
-getTheIntOrText cddl rule = case resolveIfRef cddl rule of
-  Literal (VUInt i) -> Just (Left $ fromIntegral i)
-  Literal (VText b) -> Just (Right b)
-  _ -> Nothing
-
 validateText :: CDDL' -> T.Text -> Rule' -> Validation [Reason] ()
 validateText cddl b theRule = case resolveIfRef cddl theRule of
   Literal (VText b') ->
     failUnless (b == b') [Unexpected ("the exact textstring " <> show b') (show b)]
   Postlude PTText -> _Success # ()
   Control op n ctrller ->
-    let textControl =
-          maybe
-            (_Failure # [DidNotValidate $ "Control rhs is not an int or textstring: " <> show ctrller])
-            ( runStringOp
-                "textstring"
-                op
-                b
-                T.length
-            )
-            $ getTheIntOrText cddl ctrller
+    let textControl = dispatchOp op doValidate cddl ctrller b
      in case resolveIfRef cddl n of
           Postlude PTText -> textControl
           lhs -> _Failure # [DidNotValidate $ "Control lhs is not valid for textstrings: " <> show lhs]
@@ -286,46 +181,30 @@ validateBool cddl b theRule = case resolveIfRef cddl theRule of
   Floating points
 -------------------------------------------------------------------------------}
 
-getTheHalf :: CDDL' -> Rule' -> Maybe Float
-getTheHalf cddl ref = case resolveIfRef cddl ref of
-  Literal (VFloat16 i) -> Just i
-  _ -> Nothing
-
 precHalf :: PTerm -> Validation [Reason] () -> Validation [Reason] ()
 precHalf PTHalf = id
 precHalf pt = const $ _Failure # [DidNotValidate $ "Wanted type float16 but rule is " <> show pt]
 
-getTheFloat :: CDDL' -> Rule' -> Maybe Float
-getTheFloat cddl ref = case resolveIfRef cddl ref of
-  Literal (VFloat32 i) -> Just i
-  _ -> Nothing
-
 precFloat :: PTerm -> Validation [Reason] () -> Validation [Reason] ()
 precFloat PTFloat = id
 precFloat pt = const $ _Failure # [DidNotValidate $ "Wanted type float32 but rule is " <> show pt]
-
-getTheDouble :: CDDL' -> Rule' -> Maybe Double
-getTheDouble cddl ref = case resolveIfRef cddl ref of
-  Literal (VFloat64 i) -> Just i
-  _ -> Nothing
 
 precDouble :: PTerm -> Validation [Reason] () -> Validation [Reason] ()
 precDouble PTDouble = id
 precDouble pt = const $ _Failure # [DidNotValidate $ "Wanted type float64 but rule is " <> show pt]
 
 validateFloat ::
-  (Eq a, Show a, Ord a, Num a) =>
+  (Eq a, Show a, Ord a, Num a, Ops a) =>
   CDDL' ->
   a ->
-  (CDDL' -> Rule' -> Maybe a) ->
   (PTerm -> Validation [Reason] () -> Validation [Reason] ()) ->
   Rule' ->
   Validation [Reason] ()
-validateFloat cddl i getInner checkPrecision theRule = case (resolveIfRef cddl theRule, getInner cddl theRule) of
+validateFloat cddl i checkPrecision theRule = case (resolveIfRef cddl theRule, get cddl theRule) of
   (_, Just i') ->
     failUnless (i == i') [Unexpected ("the fixed float " <> show i') (show i)]
   (Postlude pttype, Nothing) -> checkPrecision pttype (_Success # ())
-  (Range from to incl, Nothing) -> case (getInner cddl from, getInner cddl to) of
+  (Range from to incl, Nothing) -> case (get cddl from, get cddl to) of
     (Just low, Just high) ->
       failUnless
         ( low <= i && case incl of
@@ -335,18 +214,14 @@ validateFloat cddl i getInner checkPrecision theRule = case (resolveIfRef cddl t
         [Unexpected ("a float in " <> show (low, high, incl)) (show i)]
     _ -> _Failure # [DidNotValidate $ "Got malformed range " <> show (Range from to incl)]
   (Control op n ctrller, Nothing) ->
-    let floatControl =
-          maybe
-            undefined
-            (runNumericOp "float" op i)
-            $ getInner cddl ctrller
-     in case (resolveIfRef cddl n, getInner cddl n) of
+    let floatControl = dispatchOp op doValidate cddl ctrller i
+     in case (resolveIfRef cddl n, get cddl n) of
           (_, Just i') ->
             validateWithUnless
               (i == i')
               floatControl
               [Unexpected ("the fixed float " <> show i') (show i)]
-          (Range from to incl, Nothing) -> case (getInner cddl from, getInner cddl to) of
+          (Range from to incl, Nothing) -> case (get cddl from, get cddl to) of
             (Just low, Just high) ->
               validateWithUnless
                 ( low <= i && case incl of
@@ -425,19 +300,19 @@ validateItem :: CDDL' -> NE.NonEmpty Term -> Node MonoRef -> Validation [Reason]
 validateItem cddl tss rule = case (tss, resolveIfRef cddl rule) of
   (tv NE.:| ts, KV _ v _) ->
     doValidate cddl tv v
-      *> (maybe (pure ()) (\ne -> validateItem cddl ne rule) $ NE.nonEmpty ts)
+      *> (maybe (pure ()) (\next -> validateItem cddl next rule) $ NE.nonEmpty ts)
   (_, Group newRules) ->
     validateGroup cddl validateItem (NE.toList tss) newRules
   (t NE.:| ts, _) ->
     doValidate cddl t rule
-      *> (maybe (pure ()) (\ne -> validateItem cddl ne rule) $ NE.nonEmpty ts)
+      *> (maybe (pure ()) (\next -> validateItem cddl next rule) $ NE.nonEmpty ts)
 
 validateItem2 :: CDDL' -> NE.NonEmpty (Term, Term) -> Node MonoRef -> Validation [Reason] ()
 validateItem2 cddl tss rule = case (tss, resolveIfRef cddl rule) of
   ((tk, tv) NE.:| ts, KV k v _) ->
     doValidate cddl tk k
       *> doValidate cddl tv v
-      *> (maybe (pure ()) (\ne -> validateItem2 cddl ne rule) $ NE.nonEmpty ts)
+      *> (maybe (pure ()) (\next -> validateItem2 cddl next rule) $ NE.nonEmpty ts)
   (_, Group newRules) ->
     validateGroup cddl validateItem2 (NE.toList tss) newRules
   (_ NE.:| _, _) -> _Failure # [DidNotValidate $ "No group and no key-value rule in map: " <> show rule]
@@ -455,17 +330,17 @@ doValidate cddl t theRule = case resolveIfRef cddl (traceShowWith (t,) theRule) 
     doValidate cddl t r1 <* doValidate cddl t r2
   Postlude PTAny -> _Success # ()
   _ -> case t of
-    TInt i -> validateIntegral cddl i getTheInt theRule
-    TInteger i -> validateIntegral cddl i getTheInteger theRule
+    TInt i -> validateIntegral cddl i theRule
+    TInteger i -> validateIntegral cddl i theRule
     TBytes b -> validateBytes cddl b theRule
     TBytesI (BSL.toStrict -> b) -> validateBytes cddl b theRule
     TString s -> validateText cddl s theRule
     TStringI (TL.toStrict -> s) -> validateText cddl s theRule
     TTagged tg t' -> validateTag cddl tg t' theRule
     TBool b -> validateBool cddl b theRule
-    THalf h -> validateFloat cddl h getTheHalf precHalf theRule
-    TFloat f -> validateFloat cddl f getTheFloat precFloat theRule
-    TDouble d -> validateFloat cddl d getTheDouble precDouble theRule
+    THalf h -> validateFloat cddl (Half h) precHalf theRule
+    TFloat f -> validateFloat cddl f precFloat theRule
+    TDouble d -> validateFloat cddl d precDouble theRule
     TList nodes -> case resolveIfRef cddl theRule of
       Array nodes' -> validateGroup cddl validateItem nodes nodes'
       rr -> _Failure # [Unexpected "a rule for lists" (show rr)]
@@ -483,34 +358,14 @@ doValidate cddl t theRule = case resolveIfRef cddl (traceShowWith (t,) theRule) 
       rr -> _Failure # [Unexpected "a rule for nil" (show rr)]
     TSimple _s -> error "Validating TSimple unimplemented"
 
-data Reason
-  = UnboundRef Name
-  | InvalidTopCBOR
-  | InvalidInnerCBOR
-  | NotASingleTopTerm
-  | NotASingleInnerTerm
-  | DidNotValidate String
-  | Unexpected
-      -- | Expecting
-      String
-      -- | but got
-      String
-
-instance Show Reason where
-  show (UnboundRef r) = "Unbound reference " <> show r
-  show InvalidTopCBOR = "Invalid CBOR found"
-  show InvalidInnerCBOR = "Invalid CBOR in a .cbor control found"
-  show NotASingleTopTerm = "Parsed CBOR is not a single term"
-  show NotASingleInnerTerm = "Parsed CBOR in a .cbor control is not a single term"
-  show (DidNotValidate s) = "Validation failure: " <> s
-  show (Unexpected expected got) = "Expected " <> expected <> " but got " <> got
-
 validateCBOR :: BS.ByteString -> Name -> CDDL' -> Except [Reason] ()
 validateCBOR bs name ct@(CTreeRoot cddl) = do
   case Map.lookup name cddl of
     Nothing -> throwError [UnboundRef name]
     Just rule -> do
       (rest, term) <-
-        modifyError (const [InvalidTopCBOR]) $ liftEither $ deserialiseFromBytes decodeTerm (BSL.fromStrict bs)
+        modifyError (const [InvalidTopCBOR]) $
+          liftEither $
+            deserialiseFromBytes decodeTerm (BSL.fromStrict bs)
       unless (BSL.null rest) $ throwError [NotASingleTopTerm]
       liftEither $ toEither $ traceShow ct $ doValidate (traceShowId ct) term (runIdentity rule)
