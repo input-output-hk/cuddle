29c1e10c62a736a8c88c6126135de5b3f83025d7
Joosep Jääger <joosep.jaager@iohk.io>
2026-07-14 17:48:29 +0300

    Fix off by one error

--- src/Codec/CBOR/Cuddle/Huddle.hs
+++ src/Codec/CBOR/Cuddle/Huddle.hs
@@ -468,7 +468,7 @@ bool x = Literal (LBool x) mempty
 
 inferInteger :: Integer -> Literal
 inferInteger i
-  | i >= 0 && i < fromIntegral (maxBound @Word64) = Literal (LInt (fromInteger i)) mempty
+  | i >= 0 && i <= fromIntegral (maxBound @Word64) = Literal (LInt (fromInteger i)) mempty
   | Just i' <- toNInt i = Literal (LNInt i') mempty
   | otherwise = Literal (LBignum i) mempty
 
